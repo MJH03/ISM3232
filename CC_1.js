a7780f7a23315947b27a26566d4e950b0e06d81b
MJH03 <mjhandelsman@usf.edu>
2024-05-20 19:40:26 -0400

    Create and test calcTip function

--- CC_1.js
+++ CC_1.js
@@ -3,3 +3,7 @@ let Cost = 275
 const tip = Cost <= 300 && 50 >= Cost ? Cost * 0.15 : Cost * 0.2
 
 console.log (`The bill was ${Cost}, the tip is ${tip}, and the Grand Total comes to ${Cost + tip}`);
+
+let calcTip = function (Cost) {
+    return Cost <= 300 && Cost >=50 ? Cost * 0.15: Cost * 0.2;
+}
